62b6919c7bb427f00fcbd3d7ad08b5dc5dd1c250
wrotycz <kamil.weglarz@outlook.com>
2015-12-08 15:29:16 +0100

    Fixed file import issue

--- src/encountergenerator/helpers/DefaultResourceParser.java
+++ src/encountergenerator/helpers/DefaultResourceParser.java
@@ -1,7 +1,10 @@
 package encountergenerator.helpers;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.file.InvalidPathException;
@@ -9,6 +12,9 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Map;
+
+import javax.faces.context.FacesContext;
+import javax.servlet.ServletContext;
 
 import encountergenerator.interfaces.ResourceParser;
 
@@ -33,13 +39,15 @@ public class DefaultResourceParser implements ResourceParser {
 	public void setResource() {
 		if (resourceName == null)
 			return;
-
+		
 		try {
-			resourcePath = Paths.get("resources", resourceName);
+			ServletContext servletContext = (ServletContext) FacesContext
+				        .getCurrentInstance().getExternalContext().getContext();
+			resourcePath = Paths.get(servletContext.getRealPath("/"), "WEB-INF", "Classes", resourceName);
 		} catch (InvalidPathException e) {
 			System.out.println("Invalid Path error: " + e);
 		}
-
+		
 		try (BufferedReader in = new BufferedReader(
 				new InputStreamReader(new FileInputStream(resourcePath.toFile())))) {
 			String line;
