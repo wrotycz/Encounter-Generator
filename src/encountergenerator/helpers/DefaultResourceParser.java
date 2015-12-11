530f0aad187dc2b6ccf7f5f18947498fc0d2ec4c
wrotycz <kamil.weglarz@outlook.com>
2015-12-11 17:38:18 +0100

    Fixed imports on DefaultResourceParser

--- src/encountergenerator/helpers/DefaultResourceParser.java
+++ src/encountergenerator/helpers/DefaultResourceParser.java
@@ -1,10 +1,7 @@
 package encountergenerator.helpers;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.file.InvalidPathException;
